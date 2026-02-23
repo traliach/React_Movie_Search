05dd6dbdacc448c2ddb6855028d46154480de8f5
traliach <ali.achille.traore@gmail.com>
2026-02-23 16:24:57 -0500

    add component stubs

---
+++ src/components/Form.jsx
@@ -0,0 +1,8 @@
+export default function Form() {
+    return (
+        <form>
+            <input type="text" placeholder="Search for a movie" />
+            <button type="submit">Search</button>
+        </form>
+    )
+}
---
+++ src/components/MovieDisplay.jsx
@@ -0,0 +1,7 @@
+export default function MovieDisplay() {
+    return (
+        <div>
+            <h1>Movie Display</h1>
+        </div>
+    )
+}
